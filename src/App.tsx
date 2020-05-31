bbdf9e4c129cadf5b530618fc04d9b3d54d6c28e
Helder Costa <helderlpcosta@gmail.com>
2020-05-31 18:22:28 -0300

    Add global style with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Note It</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Note It</h1>
+      </div>
+      <GlobalStyle />
+    </>
   );
 };
 
