142fd1720b0b666b7168a6181cdd6a1629d847ff
Helder Costa <helderlpcosta@gmail.com>
2020-06-01 15:51:09 -0300

    Style plans card

--- src/components/NoteCard/index.tsx
+++ src/components/NoteCard/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { RiPushpinLine } from 'react-icons/ri';
 
-// import { Container } from './styles';
+import { CardContainer } from '../CardContainer';
+
+import { Note } from './styles';
 
 const NoteCard: React.FC<{ date: string; title: string; body: string; pinned?: boolean }> = ({
   date,
   title,
   body,
+  pinned,
 }) => {
   return (
-    <div>
-      <span>{date}</span>
-      <RiPushpinLine size={14} />
-      <h2>{title}</h2>
-      <p>{body}</p>
-    </div>
+    <CardContainer>
+      <Note>
+        <header>
+          <span>{date}</span>
+          {pinned && <RiPushpinLine />}
+        </header>
+        <h2>{title}</h2>
+        <p>{body}</p>
+      </Note>
+    </CardContainer>
   );
 };
 
---
+++ src/components/NoteCard/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Note = styled.div`
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #9caabf;
+
+    svg {
+      color: #9caabf;
+      opacity: 0.5;
+      width: 1.5em;
+      height: 1.5em;
+    }
+  }
+
+  h2 {
+    color: #31456a;
+    margin: 0.2em 0 0.4em 0;
+  }
+
+  p {
+    color: #576785;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
+`;
