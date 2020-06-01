feeef58cce7f0b4f2f0fbf65cd01348a868dd191
Helder Costa <helderlpcosta@gmail.com>
2020-06-01 15:50:52 -0300

    Use Card Container

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,6 +3,7 @@ import { RiAddLine, RiSearch2Line } from 'react-icons/ri';
 
 import NoteCard from '../../components/NoteCard';
 
+import { CardContainer } from '../../components/CardContainer';
 import { Container, Header, IconButton, SearchBar } from './styles';
 
 const Main: React.FC = () => {
@@ -17,16 +18,24 @@ const Main: React.FC = () => {
         </nav>
       </Header>
 
-      <SearchBar>
-        <RiSearch2Line />
-        <input type="text" placeholder="Search" />
-      </SearchBar>
+      <CardContainer>
+        <SearchBar>
+          <RiSearch2Line />
+          <input type="text" placeholder="Search" />
+        </SearchBar>
+      </CardContainer>
 
       <NoteCard
         date="12.02.19"
         title="Plants"
         body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, soluta?"
         pinned
+      />
+
+      <NoteCard
+        date="12.02.19"
+        title="Plants"
+        body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem officiis placeat eos sapiente cupiditate ab. Id nesciunt nemo deserunt culpa nobis esse inventore laborum vel ratione! Praesentium quaerat impedit earum!"
       />
     </Container>
   );
