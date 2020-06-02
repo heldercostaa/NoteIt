52e3bcfdb5fcc7ac36084f25a0e4a70769cdf659
Helder Costa <helderlpcosta@gmail.com>
2020-06-01 22:03:07 -0300

    Style modal

--- src/components/AddNoteModal/styles.ts
+++ src/components/AddNoteModal/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import ReactModal from 'react-modal';
+
 export const Container = styled.div.attrs({ role: 'dialog', 'aria-modal': true })`
   z-index: 999;
   position: absolute;
@@ -17,8 +19,6 @@ export const Container = styled.div.attrs({ role: 'dialog', 'aria-modal': true }
 `;
 
 export const Content = styled.div`
-  position: relative;
-
   width: 80%;
   height: 90%;
 
@@ -29,16 +29,17 @@ export const Content = styled.div`
   background-color: #fdfdfe;
 `;
 
+if (ReactModal.defaultStyles.overlay?.backgroundColor)
+  ReactModal.defaultStyles.overlay.backgroundColor = 'rgba(246, 246, 246, 0.84)';
 export const CloseButton = styled.button.attrs((props) => ({
   type: 'button',
   title: props.title,
 }))`
-  position: absolute;
-  top: 1rem;
-  right: 1rem;
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
 
-  padding: 0;
-  margin: 0;
+  margin: 0 0 -1em -1em;
 
   border: none;
   background: transparent;
@@ -48,5 +49,19 @@ export const CloseButton = styled.button.attrs((props) => ({
   svg {
     width: 1.5rem;
     height: 1.5rem;
+  }
+`;
+
+export const Modal = styled(ReactModal)`
+  width: 95%;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+
+  .overlay {
+    background-color: red;
   }
 `;
